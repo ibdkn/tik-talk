428e2ecabe473bac8b19f5d75bd37b048dd22f1d
ibdkn <ibudiakina@gmail.com>
2024-07-02 23:15:57 +0300

    added search page, login page, styles and logic for authorization

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,39 @@
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthService} from "../../auth/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  form = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  })
+
+  onSubmit() {
+    console.log(this.form.value)
+
+    if (this.form.valid) {
+      //todo
+      //@ts-ignore
+      this.authService.login(this.form.value)
+        .subscribe(val => {
+          this.router.navigate(['']);
+            console.log(val)
+          }
+        )
+    }
+  }
+
+}
