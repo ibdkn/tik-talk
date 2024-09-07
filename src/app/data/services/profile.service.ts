7a4fee773fe5c827b242f324f4aed3fae62684bf
ibdkn <ibudiakina@gmail.com>
2024-09-07 22:50:45 +0300

    added profile and settings

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -24,10 +24,21 @@ export class ProfileService {
       )
   }
 
-  getSubscribersShortList() {
+  getAccount(id: string) {
+    return this.http.get<Profile>(`${this.baseApiUrl}account/${id}`)
+      .pipe(
+        tap(res => this.me.set(res))
+      )
+  }
+
+  getSubscribersShortList(sumsAmount = 3) {
     return this.http.get<Pageble<Profile>>(`${this.baseApiUrl}account/subscribers/`)
       .pipe(
-        map(res => res.items.slice(0, 3))
+        map(res => res.items.slice(0, sumsAmount))
       )
+  }
+
+  patchProfile(profile: Partial<Profile>) {
+    return this.http.patch<Profile>(`${this.baseApiUrl}account/me`, profile);
   }
 }
