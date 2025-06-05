7ee4199075fa42a967de577cfca935d6fc9b2a4d
irinabudiakina <ibudiakina@gmail.com>
2025-06-05 19:03:35 +0300

    added the first part

---
+++ src/app/data/services/profile.service.ts
@@ -0,0 +1,18 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Profile} from '../interfaces/profile.interface';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  http: HttpClient = inject(HttpClient);
+  baseApiUrl: string = 'https://icherniakov.ru/yt-course';
+
+  constructor() { }
+
+  getTestAccounts(): Observable<Profile[]> {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}/account/test_accounts`)
+  }
+}
