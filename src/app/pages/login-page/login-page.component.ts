52af7d00f98b907a8ebd24e171b5e8d644ff29ef
irinabudiakina <ibudiakina@gmail.com>
2025-06-07 18:07:22 +0300

    added signal for eye-btn

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal, WritableSignal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../auth/auth.service';
 import {Login} from '../../data/interfaces/login.interface';
@@ -16,12 +16,14 @@ export class LoginPageComponent {
   authService: AuthService = inject(AuthService);
   router: Router = inject(Router);
 
+  isPasswordVisible: WritableSignal<boolean> = signal<boolean>(false);
+
   form: FormGroup = new FormGroup<{ username: FormControl<string>; password: FormControl<string> }>({
     username: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
     password: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
   })
 
-  onSubmit() {
+  onSubmit(): void {
     console.log(this.form.value)
     if (this.form.valid) {
       this.authService.login(this.form.value)
